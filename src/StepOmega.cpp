b039eb41eb6bcf0dd9babf09240f101fefc8af8d
srijata06 <84949702+srijata06@users.noreply.github.com>
2021-12-20 11:22:04 +0530

    Update StepOmega.cpp
    Allowing the option of positive definiteness check and beta hyperpriors on prior weights

--- src/StepOmega.cpp
+++ src/StepOmega.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 
 // [[Rcpp::export]]
-List StepOmega(mat S, mat temp, int nmc, int burnin, int n, double r = 1e-4, double s = 1e-8){
+List StepOmega(mat S, mat temp, int nmc, int burnin, int n, double r = 1e-4, double s = 1e-8, double alpha2 = 1, double beta2 = 1,int posdef = 0, int hyp = 0){
 	
   int p = S.n_cols;
   mat Omega = zeros(p,p);
@@ -24,6 +24,8 @@ List StepOmega(mat S, mat temp, int nmc, int burnin, int n, double r = 1e-4, dou
   cube OmegaCount = zeros(p,p,2); // OmegaCount is an array of the votes given to zero and non-zero edges
   vec P = zeros(2);
   vec myvec = zeros(2);
+  vec mhvec = zeros(2);
+  vec probdiag = zeros(2);
   for(int t=0; t<2;t++)
     myvec(t) = t;
   
@@ -32,14 +34,25 @@ List StepOmega(mat S, mat temp, int nmc, int burnin, int n, double r = 1e-4, dou
   double bd = 0;
   double lambda = 0;
   double q2 = 1-(1/(double)p);
+  double v;
+  double rho;
+  double nomega;
   int rand = 1;
   mat OmegaSum = zeros(p,p);
+  mat I = eye(p,p);
+    
   clock_t time_start, time_end;
   time_start = clock();
   
   
 
   for(int it = 0; it < (nmc + burnin+1); it++){
+	
+    if( hyp == 1){
+		uvec idx = find(Omega != 0);
+		nomega = 0.5*(idx.size() - (double)p);
+	    q2 = R::rbeta(0.5 * (double)p * ((double)p -1) - nomega + alpha2, nomega + beta2);
+	}
 
     for(int j=0; j<(p-1); j++){
       for(int k=(j+1); k<p; k++){
@@ -48,20 +61,23 @@ List StepOmega(mat S, mat temp, int nmc, int burnin, int n, double r = 1e-4, dou
         b = - sum(Omega.col(j)%S.col(k) + Omega.col(k)%S.col(j)) + Omega(j,k)*a;
 		
         if((Omega(j,k) == 0)){
-		 lambda = R::rgamma(r, s);   
+		 lambda = R::rgamma(r, s);
+         	 
 	    }
 		else{
 		lambda = R::rgamma(r + 0.5, 1/(0.5*Omega(j,k)*Omega(j,k) + s));   
+		
 		}
-                if(lambda == 0){
-			P(1) = 0;
-		}else{
-		P(1) = sqrt(lambda/(n*a+lambda)) * exp((n*b)*(n*b)/(2*(n*a+lambda))) *(1-q2)/q2;
-		}
+        
+        if(lambda == 0)	{
+            P(1) = 0;
+	    }  
+	    else{
+        P(1) = sqrt(lambda/(n*a+lambda)) * exp((n*b)*(n*b)/(2*(n*a+lambda))) *(1-q2)/q2; 
+	    }
         P(0) = 1;
 		
-	
-        if(P.has_inf()==true){
+	    if(P.has_inf()==true){
         	
           rand = 1;
           Omega(j,k) = Omega(k,j) = R::rnorm(n*b/(n*a+lambda), 1/sqrt(n*a + lambda));
@@ -93,8 +109,10 @@ List StepOmega(mat S, mat temp, int nmc, int burnin, int n, double r = 1e-4, dou
             OmegaSum(j,k) +=  Omega(j,k);
             OmegaSum(k,j) += Omega(k,j);
 			}
-            	      
-    	   } else {
+            
+    	      
+    	       
+          } else {
             Omega(j,k) = Omega(k,j) = 0;
           }
      
@@ -109,20 +127,59 @@ List StepOmega(mat S, mat temp, int nmc, int burnin, int n, double r = 1e-4, dou
       }
       
       bd = sum(Omega.col(j)%S.col(j)) - Omega(j,j)*S(j,j) + (R::rgamma(r + 1, 1/(abs(Omega(j,j)) + s)))/n;
-      Omega(j,j) = (sqrt(bd*bd + 4*S(j,j)) - bd)/(2*S(j,j));
+        
+	  double mode = (sqrt(bd*bd + 4*S(j,j)) - bd)/(2*S(j,j));
+	  v = R::rnorm(mode, 0.001);
+	  rho = std::min(exp(n * (log(v) - log(Omega(j,j))) - 0.5 * S(j,j) * ((v*v) - (Omega(j,j)*Omega(j,j))) - bd * (v - Omega(j,j))),1.0);
 	  
+	  mhvec(0) = v;
+	  mhvec(1) = Omega(j,j);
+	  probdiag(0) = rho;
+	  probdiag(1) = 1-rho;
+	  
+	  Omega(j,j) = (Rcpp::RcppArmadillo::sample(mhvec,1,false,probdiag))(0);
+	
       if(it > burnin)
         OmegaSum(j,j) += Omega(j,j); 
+	  
+		
     }
     bd = sum(Omega.col(p-1)%S.col(p-1)) - Omega(p-1,p-1)*S(p-1,p-1) + R::rgamma(r + 1, 1/(abs(Omega(p-1, p-1)) + s))/n;
-    Omega(p-1,p-1) = (sqrt(bd*bd + 4*S(p-1,p-1)) - bd)/(2*S(p-1,p-1));
+    	
+	double mode = (sqrt(bd*bd + 4*S(p-1,p-1)) - bd)/(2*S(p-1,p-1));
+	v = R::rnorm(mode, 0.001);
+	rho = std::min(exp(n * (log(v) - log(Omega(p-1,p-1))) - 0.5 * S(p-1,p-1) * ((v*v) - (Omega(p-1,p-1)*Omega(p-1,p-1))) - bd * (v - Omega(p-1,p-1))),1.0);
+	
+	mhvec(0) = v;
+	mhvec(1) = Omega(p-1,p-1);
+	probdiag(0) = rho;
+	probdiag(1) = 1-rho;
+		
+	Omega(p-1,p-1) = (Rcpp::RcppArmadillo::sample(mhvec,1,false,probdiag))(0);
+	
 	if(it > burnin)
       OmegaSum(p-1,p-1) += Omega(p-1,p-1);
+		
+	    if( posdef == 1){
+			
+            double eigmin = min(eig_sym(Omega));
+			
+	        if(eigmin < 0){
+	            
+                Rcout << "Omega is not pd in iteration : " << it <<"\n";	   
+	            mat Omeganew = Omega - (eigmin + 0.01) * I;
+    	
+		        if(it > burnin){
+		            for(int j=0; j<p; j++){
+		                OmegaSum(j,j) = OmegaSum(j,j) - Omega(j,j) + Omeganew(j,j);
+	                }
+		        }
+	            Omega = Omeganew;
+	        }
+	    }
+	
     
-    
-    
-   
-   if(it - floor(it/1000)*1000 == 0){
+     if(it - floor(it/1000)*1000 == 0){
       vec iteration = ones(1)*it;
       iteration.print();
     }
