a410db518029bb7f118901b43f8bcc48d3498c3d
srijata06 <84949702+srijata06@users.noreply.github.com>
2021-12-20 11:21:05 +0530

    Update StepB.cpp
    Updating function to include the option of hyperpriors on prior weights

--- src/StepB.cpp
+++ src/StepB.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 
 // [[Rcpp::export]]
-List StepB(mat B_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, double s1 = 1e-8){
+List StepB(mat B_ini, vec sigma2_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, double s1 = 1e-8,double alpha1 = 1, double beta1 = 1, int hyp = 0){
 	
 	int n = Y.n_rows;
 	int p = B_ini.n_rows;
@@ -26,8 +26,7 @@ List StepB(mat B_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, doubl
 	B = B_ini ;
 	double tau;
 	double eta;
-	double p1 = (double)p;
-    double q1 = 1-(1/p1);
+	double q1 = 1-(1/(double)p);
 	double nb ;
 	vec resid =zeros(p);
 	double beta ;
@@ -35,9 +34,11 @@ List StepB(mat B_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, doubl
 	double b;
 	double c2 ;
 	
+	
+	
 	int components = 1;
 	vec P_B = zeros(2);
-	vec var_mod = zeros(q);
+	vec var_mod = sigma2_ini;
 	cube Bcount = zeros(p,q,2);
     mat CI_lower = zeros(p,q);
 	mat CI_upper = zeros(p,q);
@@ -45,8 +46,7 @@ List StepB(mat B_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, doubl
 	mat Bsum = zeros(p,q);
 	cube out = zeros(p,q,nmc+1);
 	vec myvec = zeros(2);
-	mat se = zeros(p,q);
-	mat invrs = zeros(p,q);
+	
 	clock_t time_start, time_end;
     time_start = clock();
     
@@ -57,13 +57,15 @@ List StepB(mat B_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, doubl
 	for(int it = 0; it < (nmc + burnin+1); it++){
 	 	 	
 	    for(int k=0; k<q; k++){
-	    	
-	    	
-	    	uvec idx = find(B.col(k) != 0);
-	    	nb = idx.size();
+			
+			uvec idx = find(B.col(k) != 0);
+		    nb = idx.size();
 	    	resid = Y.col(k) - X*B.col(k);
-            
-            
+	    	
+	    	if(hyp == 1){
+				q1 = R::rbeta((double)p * (double)q - nb + alpha1, nb + beta1);
+			}
+	    	
            for(int j=0; j<p; j++){
            	
 			   b = XtX(j,j) ;
@@ -71,21 +73,24 @@ List StepB(mat B_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, doubl
 			   
 			   if((B(j,k) == 0)){
 				eta = R::rgamma(r1, s1);   
+				
 			   }
 			   else{
-				eta = R::rgamma(r1 + 0.5, 1/(0.5*B(j,k)*B(j,k) + s1));   
+				eta = R::rgamma(r1 + 0.5, 1/((0.5*B(j,k)*B(j,k)/var_mod(k)) + s1));  
+                	
 			   }
                
-			if(eta == 0) {
+			   if(eta == 0) {
 				   P_B(1) = 0;
 			   }
 			   else {
 				   P_B(1) = sqrt(eta/(b + eta)) * exp(c2*c2/(2*(b + eta))) *(1-q1)/q1; 
 			   }
-            P_B(0) = 1;
+			   
+			P_B(0) = 1;
             tau = 1/sqrt(eta);
 			c1 = b + eta;
-			beta = s1 + 0.5*sum(resid % resid) + sum(B.col(k) % B.col(k))/(2 * tau*tau);
+			beta =  s1 + 0.5*sum(resid % resid) + sum(B.col(k) % B.col(k))/(2 * tau*tau);
             var_mod(k) = R:: rgamma( r1 + (nb/2) + (n/2), 1/beta);
             var_mod(k) = 1/var_mod(k);
 			   
@@ -139,6 +144,7 @@ List StepB(mat B_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, doubl
 		    
 		    
 	}
+	mat Bcount0 = Bcount.slice(0);
 	
     
     for (int mi = 0 ; mi < p ; mi++){
@@ -154,6 +160,7 @@ List StepB(mat B_ini, mat Y, mat X, int nmc, int burnin, double r1 = 1e-4, doubl
            }
         }
     }
+	
 	time_end = clock();
     double time_taken;
     time_taken = (time_end - time_start) / (double)CLOCKS_PER_SEC;
